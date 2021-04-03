b5d18918246ee93b59dd6a2a6a62004fafa0590e
Abhishek Kumar <gabhishekkumar@outlook.com>
2021-04-03 15:15:59 +0530

    Initial commit

---
+++ src/main/java/com/abhinotes/gcp/sentiments/SentimentsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.abhinotes.gcp.sentiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abhinotes/gcp/sentiments/SentimentsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinotes.gcp.sentiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
